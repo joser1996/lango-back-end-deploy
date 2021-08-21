aceba2a841eadb2c20ecc26f7a912b71a6d9986c
Jose Torres <josertrs@gmail.com>
2021-08-20 18:05:54 -0700

    Fixed; was mongodb issue had to whitelist heroku

--- server.js
+++ server.js
@@ -214,14 +214,3 @@ app.get('/test/db', (req, res) => {
     });
 });
 
-// server.get('/usersList', function(req, res) {
-//     User.find({}, function(err, users) {
-//       var userMap = {};
-  
-//       users.forEach(function(user) {
-//         userMap[user._id] = user;
-//       });
-  
-//       res.send(userMap);  
-//     });
-//   });
